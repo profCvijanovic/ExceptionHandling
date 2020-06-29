9c15e79a8508ff670dc70f60b8b9f2222a2cea61
Korisnik <Korisnik@192.168.194.250>
2020-06-29 21:26:01 +0200

    Exception handling using if else structure

--- ExceptionHandling/src/izuzeci/ExceptionGlavna.java
+++ ExceptionHandling/src/izuzeci/ExceptionGlavna.java
@@ -7,6 +7,26 @@ public class ExceptionGlavna {
 		
 		System.out.println("Pozdrav!");
 		System.out.println("Sada cemo da radimo exceptione!");
+		
+		
+		int broj1 = 10;
+		int broj2 = 0;
+		
+		// Exception handling koristeci if else strukturu
+		if(broj2 != 0) {
+			int kolicnik = broj1/broj2;
+			System.out.println("Kolicnik je: " + kolicnik);
+		}else {
+			System.out.println("Zabranjeno je deliti sa nulom!!!");
+		}
+		
+		System.out.println("Nastavak....");
+		
+		
+		
+		
+		
+		
 
 	}
 
