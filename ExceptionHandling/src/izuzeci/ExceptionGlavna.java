1616aeab8cb7a3df76bcf64fae605ceb4f29eb50
Korisnik <Korisnik@192.168.194.250>
2020-07-01 20:48:01 +0200

    resavanje exceptiona- muko moja predji na drugog

--- ExceptionHandling/src/izuzeci/ExceptionGlavna.java
+++ ExceptionHandling/src/izuzeci/ExceptionGlavna.java
@@ -1,40 +1,20 @@
 package izuzeci;
 
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
-
 public class ExceptionGlavna {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Unesite broj artikla: ");
-		System.out.println("0,1 ili 2");
-		Scanner scanner = new Scanner(System.in);
+
+		int broj1 = 10;
+		int broj2 = 0;
 		
 		
-		List<String> korpa = new ArrayList<String>();
-			korpa.add("mis");
-			korpa.add("tastatura");
-			korpa.add("monitor");
-	
 		try {
-			int broj = scanner.nextInt();
-			String izabraniArtikal = korpa.get(broj);
-			System.out.println("Izabrali ste: " + izabraniArtikal);
-			System.out.println("Kraj");
-		}catch (Exception e) {
-			System.out.println("Ovo pokriva sve ostale exceptione");
-			System.out.println("Kraj");
-			e.printStackTrace();
-		}finally {
-			scanner.close();
-			System.out.println("Zatvoren skener");
+			System.out.println("Podelio sam!");
+			System.out.println(RacunskeOperacije.divide(broj1, broj2));
+		} catch (Exception e) {
+			System.out.println("Ne mozete da delite sa 0");
 		}
-		
-		
-		
 		
 		
 		
---
+++ ExceptionHandling/src/izuzeci/RacunskeOperacije.java
@@ -0,0 +1,11 @@
+package izuzeci;
+
+public class RacunskeOperacije {
+	
+	public static double divide(int broj1, int broj2) throws Exception {
+		
+		double rezultat = broj1/broj2;
+		return rezultat;
+	}
+
+}
